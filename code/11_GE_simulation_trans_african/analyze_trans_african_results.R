3d18894e8197e4be631c8e672d273ccdc419ce37
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-09-03 13:54:01 +0200

    Need dot.

--- code/11_GE_simulation_trans_african/analyze_trans_african_results.R
+++ code/11_GE_simulation_trans_african/analyze_trans_african_results.R
@@ -10,7 +10,7 @@ fastverse_conflicts()
 
 net <- "fastest_routes" # Or 'all_routes' to also include shortest routes
 dir <- "trans_african"  # Or 'trans_african_add' to get shortest routes results  
-res_name <- "22g_10b_fixed_cgc_sigma38_rho0_julia" # '22g_add_10b_fixed_duality_sigma3'
+res_name <- "22g_10b_fixed_cgc_sigma3.8_rho0_julia" # '22g_add_10b_fixed_duality_sigma3'
 
 results <- list(
   nodes = fread(sprintf("results/transport_network/GE/%s/nodes_results_%s.csv", dir, res_name)),
